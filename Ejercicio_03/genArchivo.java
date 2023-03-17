50e9017d63e4a3795a4eea1e3077f7c60d9b5263
Leon E. Prieto Bailo <leprieto@indra.es>
2023-03-17 10:20:22 +0100

    Añadida la fecha al nombre del archivo.

--- Ejercicio_03/genArchivo.java
+++ Ejercicio_03/genArchivo.java
@@ -4,14 +4,21 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class genArchivo implements interfaceGenArchivos {
 
   @Override
   public void generarArchivo(empleado[] empleados) {
     try {
-      String ruta =
-          "C:\\Users\\leprieto\\Documents\\Curso Java\\CursoJava\\Ejercicio_03\\output.txt";
+      String ruta = "C:\\Users\\leprieto\\Documents\\Curso Java\\CursoJava\\Ejercicio_03\\";
+
+
+      LocalDate fecha = LocalDate.now();
+      DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd_MM_YYYY");
+      String fechaFormateada = fecha.format(dateTimeFormatter);
+      String rutaCompleta = ruta + "info_empleados_" + fechaFormateada;
 
 
       String contenido = "NOMBRE APELLIDO LOCALIDAD POSICION SUELDO\n";
@@ -25,8 +32,7 @@ public class genArchivo implements interfaceGenArchivos {
       }
 
 
-      File archivo = new File(ruta);
-      // Si el archivo no existe es creado.
+      File archivo = new File(rutaCompleta);
       if (!archivo.exists()) {
         archivo.createNewFile();
       }
